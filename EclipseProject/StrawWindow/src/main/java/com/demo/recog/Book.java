10401d69b7cebd9a4eeb50f866719000fc877faa
Aditya Singh <adityasinghwolvieait@gmail.com>
2018-03-17 23:17:49 +0530

    Speech Engine Updated

---
+++ EclipseProject/StrawWindow/src/main/java/com/demo/recog/Book.java
@@ -0,0 +1,71 @@
+package com.demo.recog;
+
+import java.net.URL;
+
+import edu.cmu.sphinx.frontend.util.Microphone;
+import edu.cmu.sphinx.recognizer.Recognizer;
+import edu.cmu.sphinx.result.Result;
+import edu.cmu.sphinx.util.props.ConfigurationManager;
+import edu.cmu.sphinx.util.props.PropertyException;
+
+public class Book 
+{
+	public static void main(String[] args) throws InterruptedException 
+	{
+		
+        try 
+        {
+        	URL url = Book.class.getResource("book.config.xml");
+
+            System.out.println("Loading...");
+
+            ConfigurationManager cm = new ConfigurationManager(url);
+            
+            Recognizer recognizer = (Recognizer) cm.lookup("recognizer");
+            Microphone microphone = (Microphone) cm.lookup("microphone");
+
+
+            /* allocate the resource necessary for the recognizer */
+            recognizer.allocate();
+            
+            /* the microphone will keep recording until the program exits */
+            if (microphone.startRecording()) 
+            {
+
+            	while (true) 
+            	{
+            		System.out.println("Start speaking. Press Ctrl-C to quit.\n");
+
+                    /*
+                     * This method will return when the end of speech
+                     * is reached. Note that the endpointer will determine
+                     * the end of speech.
+                     */ 
+            		Result result = recognizer.recognize();
+		    
+            		if (result != null) 
+            		{
+            			String resultText = result.getBestFinalResultNoFiller();
+            			
+            			System.out.println("You said: " + resultText + "\n");
+            		} 
+            		else 
+            		{
+            			System.out.println("I can't hear what you said.\n");
+            		}
+            	}
+            } 
+            else 
+            {
+            	System.out.println("Cannot start microphone.");
+            	recognizer.deallocate();
+            	System.exit(1);
+            }
+        } 
+        catch (PropertyException e) 
+        {
+            System.err.println("Problem configuring HelloWorld: " + e);
+            e.printStackTrace();
+        }
+    }
+}
